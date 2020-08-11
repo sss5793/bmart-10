b2e55efa9a47ba1a4e1eadb2f18c90d9cf184a12
changicho <changicho29@gmail.com>
2020-08-11 17:00:02 +0900

    feat: initial main file

---
+++ commit-convention.js
@@ -0,0 +1,38 @@
+module.exports = {
+  parserPreset: "conventional-changelog-conventionalcommits",
+  rules: {
+    "body-leading-blank": [1, "always"],
+    "body-max-line-length": [2, "always", 80],
+    "footer-leading-blank": [1, "always"],
+    "footer-max-line-length": [2, "always", 80],
+    "header-max-length": [2, "always", 52],
+    "scope-case": [2, "always", "lower-case"],
+    "subject-case": [
+      2,
+      "never",
+      ["sentence-case", "start-case", "pascal-case", "upper-case"],
+    ],
+    "subject-empty": [2, "never"],
+    "subject-full-stop": [2, "never", "."],
+    "type-case": [2, "always", "lower-case"],
+    "type-empty": [2, "never"],
+    "type-enum": [
+      2,
+      "always",
+      [
+        "!HOTFIX",
+        "chore",
+        "comment",
+        "design",
+        "docs",
+        "feat",
+        "fix",
+        "refactor",
+        "remove",
+        "rename",
+        "style",
+        "test",
+      ],
+    ],
+  },
+};
