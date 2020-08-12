e887ac84a72c3f0feb1bae3bf53d40f38c652ed4
changicho <changicho29@gmail.com>
2020-08-12 16:47:14 +0900

    fix: serviceWorker 파일 제거
    사용하지 않는 serviceWorker 파일을제거함
    
    이는 unused-var 에러로 인해 실행되지 않기 때문

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,7 +5,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./components/App";
-import * as serviceWorker from "./serviceWorker";
 
 import "./stylesheets/index.css";
 
@@ -15,7 +14,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below.
-serviceWorker.unregister();
