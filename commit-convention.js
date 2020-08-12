a98d2e871813a1b4696615408522a2b0125c31c1
changicho <changicho29@gmail.com>
2020-08-12 15:59:25 +0900

    chore: 커밋 컨벤션 수정
    한글로 작성한 커밋도 사용할 수 있도록 subject-case 옵션 삭제

--- commit-convention.js
+++ commit-convention.js
@@ -7,11 +7,6 @@ module.exports = {
     "footer-max-line-length": [2, "always", 80],
     "header-max-length": [2, "always", 52],
     "scope-case": [2, "always", "lower-case"],
-    "subject-case": [
-      2,
-      "never",
-      ["sentence-case", "start-case", "pascal-case", "upper-case"],
-    ],
     "subject-empty": [2, "never"],
     "subject-full-stop": [2, "never", "."],
     "type-case": [2, "always", "lower-case"],
