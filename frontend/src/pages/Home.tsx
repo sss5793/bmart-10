caba5408cb86822d06f945af3a21aece8355c844
이슬기 <tmfrl002@gmail.com>
2020-08-13 17:26:40 +0900

    fix: import 오류 수정
    - 잘못된 경로 수정

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import Layout from '../components/layout';
-import Category from "../components/CategoryButtonArea";
+import Layout from '../components/common/Layout';
+// import Category from "../components/CategoryButtonArea";
 
 const Home = (): JSX.Element => {
   return (
-    <Layout>메인 페이지
-      <Category></Category>
-    </Layout>);
+    <Layout>
+      메인 페이지
+      {/* <Category></Category> */}
+    </Layout>
+  );
 };
 
 export default Home;
