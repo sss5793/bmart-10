012bd2b5efb4006f9c7e1e9070fc126103f9cce3
brightchul <abhbtbb@gmail.com>
2020-08-13 12:56:27 +0900

    rename: 디렉토리 이름변경으로 인한 경로 변경

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Layout from '../components/layout';
-import Category from "../components/categoryButtonArea";
+import Category from "../components/CategoryButtonArea";
 
 const Home = () => {
   return (
