ad6dd5bf4778ca4ca1e4f17d110f2418ef86f427
brightchul <abhbtbb@gmail.com>
2020-08-13 16:46:43 +0900

    rename: 변경된 디렉토리 구조로 파일 이동, 상수 설정
    - 변경된 디렉토리 구조 components/home/로 이동
    - message에 관련 상수 정리

---
+++ frontend/src/components/home/CategoryButtonArea/CategoryButton.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import styled, { keyframes } from "styled-components";
+import { KEY_NAME } from "../../../constatns/message";
+
+
+type KeyName = {
+    [key: string]: { [name: string]: string };
+}
+
+const smallOut = keyframes`
+  0% {
+      transform: scale(1);
+  }
+  50% {
+      transform: scale(0.8);
+  }
+  100% {
+      transform: scale(1);
+  }
+`;
+
+const ButtonWrapper = styled.div`
+    margin-top:10px;
+    text-align:center;
+    width:65px;
+    height:65px;
+
+    &:active {
+        animation-duration: 0.3s;
+        animation-timing-function: ease-out;
+        animation-name: ${smallOut};
+    }
+`;
+
+const ButtonImgWrapper = styled.div`
+    border-radius: 50%;
+    width:48px;
+    height:48px;
+    margin:0 auto;
+    border:1px solid black;
+`;
+
+const ButtonImg = styled.img`
+    width:48px;
+    padding:10px;
+`;
+
+const ButtonTitle = styled.p`
+    margin-top:5px;
+    font-size:0.7em;
+`;
+
+function CategoryButton({ key }: { key: string }): JSX.Element {
+    const imagePath = `/asset/icon/${key}.svg`;
+
+    return (
+        <ButtonWrapper>
+            <div>
+                <ButtonImgWrapper>
+                    <ButtonImg src={imagePath}></ButtonImg>
+                </ButtonImgWrapper>
+            </div>
+            <ButtonTitle>
+                {KEY_NAME[key].name}
+            </ButtonTitle>
+        </ButtonWrapper >
+    );
+}
+
+export default CategoryButton;
