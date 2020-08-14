170e18f66496724423df3ba3ac51fd3f6e7cf87d
이슬기 <tmfrl002@gmail.com>
2020-08-14 14:00:49 +0900

    !HOTFIX
    - 페이지 라우팅 동작 오류 이전 버전으로 원복
    - 불필요한 콘솔 삭제

--- frontend/src/components/home/MainItem/index.tsx
+++ frontend/src/components/home/MainItem/index.tsx
@@ -1,21 +1,21 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 
 import ItemImg from './ItemImg';
 import ItemContent from './ItemContent';
 const ItemArea = styled.div`
-    margin-top:15px;
-    width: 107px;
+  margin-top: 15px;
+  width: 107px;
 `;
 
 export default function MainItem(props: any): JSX.Element {
-    console.log(props);
-    const { title, price, sale, width, src } = props;
-    const style = { width: width + "px" };
-    return (
-        <ItemArea style={style}>
-            <ItemImg width={width} src={src}></ItemImg>
-            <ItemContent title={title} price={price} sale={sale}></ItemContent>
-        </ItemArea >
-    );
+  // console.log(props);
+  const { title, price, sale, width, src } = props;
+  const style = { width: width + 'px' };
+  return (
+    <ItemArea style={style}>
+      <ItemImg width={width} src={src}></ItemImg>
+      <ItemContent title={title} price={price} sale={sale}></ItemContent>
+    </ItemArea>
+  );
 }
