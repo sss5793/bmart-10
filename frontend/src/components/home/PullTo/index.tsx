822019ee0a5ee666ff0b4ce69b6e0189ebf583db
changicho <changicho29@gmail.com>
2020-08-18 16:23:04 +0900

    feat: 당겨요 컴포넌트 제작
    외부에서 전달받은 height 값을 토대로 자기 자신의 height style 값
    을 변경해 당긴 이후를 보여주는 컴포넌트 추가
    
    related issue: #54

---
+++ frontend/src/components/home/PullTo/index.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  outline: none;
+`;
+
+const Text = styled.div`
+  color: #000;
+  font-size: 20px;
+`;
+
+type Props = {
+  observable: {
+    callbacks: Function[];
+    trigger: (height: number) => void;
+  };
+};
+
+export default function PullTop(props: Props): JSX.Element {
+  const [height, setHeight] = useState(0);
+
+  props.observable.callbacks.push(setHeight);
+
+  return (
+    <Wrapper style={{ height: height }}>
+      <Text>땡겨도 아무것도 없어요</Text>
+    </Wrapper>
+  );
+}
