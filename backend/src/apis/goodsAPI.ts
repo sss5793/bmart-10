f4719d5a9af8e1dfd5f2c23e498f46ce70e9da56
changicho <changicho29@gmail.com>
2020-08-18 22:49:47 +0900

    docs: 상품 검색 API 주석 작성
    apidoc 양식에 따라 상품 검색 api의 설명을 작성함
    
    related issue: #61

--- backend/src/apis/goodsAPI.ts
+++ backend/src/apis/goodsAPI.ts
@@ -5,6 +5,33 @@ import goodsDAO from "../daos/goods.dao";
 
 const router = Router();
 
+/**
+ * @api {get} /api/goods/:name 상품 이름으로 검색
+ * @apiName Goods by Name
+ * @apiGroup Goods
+ *
+ * @apiParam {String} name goods title
+ *
+ * @apiSuccess {Boolean} success API 성공 여부
+ * @apiSuccess {Object} data 검색한 이름이 포함된 상품 리스트
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "success": true,
+ *       "data" : {
+ *         "rows" : Goods[]
+ *       }
+ *     }
+ *
+ * @apiError NotFound
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "success": false,
+ *     }
+ */
 router.get("/:name", async (request: Request, response: Response) => {
   const { name } = request.params;
   const apiResponse: APIResponse = {
