97ed9fb9793d41a5461a924bbd41411905e65628
brightchul <abhbtbb@gmail.com>
2020-08-20 00:19:27 +0900

    feat: Fold 컴포넌트 추가
    - 접었다가 펼칠수 있는 Fold 컴포넌트를 common에 추가함
    - title 속성에 상단 부분 글자를 입력
    - children에는 펼쳤을 때의 엘리먼트를 넣으면 된다.

--- frontend/src/components/common/index.ts
+++ frontend/src/components/common/index.ts
@@ -1,5 +1,6 @@
 import Layout from "./Layout";
 import SVG from "./Svg";
 import HorizontalSlider from "./Slider/Horizontal";
+import Fold from "./Fold";
 
-export { Layout, SVG, HorizontalSlider };
+export { Layout, SVG, HorizontalSlider, Fold };
