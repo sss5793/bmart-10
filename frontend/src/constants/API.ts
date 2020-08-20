7151f9bd7a73abb45da157f5927505e1ea0db825
changicho <changicho29@gmail.com>
2020-08-20 17:10:17 +0900

    feat: API 응답, 상품정보 타입 정의
    API 제작에 사용할 타입들 정의
    
    공통 API 응답 타입과 상품 정보에 대한 타입을 정의함
    이는 backend 의 코드와 동일

---
+++ frontend/src/constants/API.ts
@@ -0,0 +1,4 @@
+export const HOST = process.env.REACT_APP_API_HOST;
+export const PORT = process.env.REACT_APP_API_PORT;
+
+export const API_HOST = `http://${HOST}:${PORT}`;
---
+++ frontend/src/types/Goods.ts
@@ -0,0 +1,9 @@
+export type Goods = {
+  id?: number;
+  name: string;
+  categoryName: string;
+  cost: number;
+  discount: number;
+  amount: number;
+  imageUrl: string;
+};
