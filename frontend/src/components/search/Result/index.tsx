cb740e3c7eed9b6fbbae6ed5111eb01b1542ff2d
changicho <changicho29@gmail.com>
2020-08-20 20:13:28 +0900

    feat: 검색 결과 리스트 출력
    이전에 작성한 이미지와 검색후에 받아온 결과를 바탕으로 결과를
    화면에 뿌려주는 기능추가
    
    related issue: #83

--- frontend/src/components/search/Result/index.tsx
+++ frontend/src/components/search/Result/index.tsx
@@ -3,20 +3,30 @@ import styled from "styled-components";
 
 import { useSearchState } from "../../../contexts/SearchContext";
 
+import GoodInformation from "./GoodInformation";
+
 const Wrapper = styled.div`
   z-index: 1000;
-  position: fixed;
   padding: 10px;
 
   width: 100%;
+
+  overflow-y: scroll;
 `;
 
 export default function SearchBar(): JSX.Element {
   const state = useSearchState();
   return (
     <Wrapper>
-      {state.searchResult.map((cur) => {
-        return `${cur.name}, ${cur.amount}`;
+      {state.searchResult.map((cur, index) => {
+        return (
+          <GoodInformation
+            key={`${cur.name}/${index}`}
+            title={cur.name}
+            price={cur.cost}
+            imageURL={cur.imageUrl}
+          />
+        );
       })}
     </Wrapper>
   );
