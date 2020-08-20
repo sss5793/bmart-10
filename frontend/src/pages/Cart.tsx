36e06453ced393fb118d9b9f31e9107026b8299d
이슬기 <tmfrl002@gmail.com>
2020-08-20 20:37:25 +0900

    style: 프론트엔드 장바구니 페이지
    - CartHeader, ShadowBar, CartList, CartToTal, OrderBtn, NotCartItem 컴포넌트 추가

--- frontend/src/pages/Cart.tsx
+++ frontend/src/pages/Cart.tsx
@@ -1,8 +1,42 @@
-import React from 'react';
-import { Layout } from '../components/common';
+import React from "react";
+import style from "styled-components";
+import { Layout, ShadowBar } from "../components/common";
+import {
+  OrderBtn,
+  CartHeader,
+  CartList,
+  CartTotal,
+  NotCartItem,
+} from "../components/cart";
+import { getItems } from "../mock";
+
+const FakeComp = style.div`
+  height: 80px;
+`;
 
 const Cart = (): JSX.Element => {
-    return <Layout>장바구니 페이지</Layout>;
+  const isCart = false; // 장바구니에 담긴 상품이 있는지 여부
+  // totalPrice, deliveryTips - context api
+  // cartItemList - context api
+  const data = getItems(3);
+
+  return (
+    <Layout>
+      {isCart ? (
+        <>
+          <CartHeader />
+          <ShadowBar />
+          <CartList data={data} />
+          <ShadowBar />
+          <CartTotal />
+          <FakeComp />
+          <OrderBtn />
+        </>
+      ) : (
+        <NotCartItem />
+      )}
+    </Layout>
+  );
 };
 
 export default Cart;
