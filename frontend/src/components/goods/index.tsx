954eb4fc2bbae941967fa411bbd954ff718421c3
brightchul <abhbtbb@gmail.com>
2020-08-20 12:02:02 +0900

    feat: 배달, 교환  반품 정보, 상세페이지 하단부 작업
    - 배달, 교환 반품 정보 컴포넌트 작업
    - 하단부 구매하기 버튼 컴포넌트
    - 구매하기 컴포넌트 클릭시 아래에서 나타나는 장바구니 담기 컴포넌트
    - 스타일 컴포넌트를 따로 분리한 다음 import

--- frontend/src/components/goods/index.tsx
+++ frontend/src/components/goods/index.tsx
@@ -1,11 +1,27 @@
-import React from "react";
-import { getItem } from "../../mock";
+import React, { useState } from "react";
+import {
+  EtcInfo,
+  ButtonArea,
+  Button,
+  Shadow,
+  HideArea,
+  HideAreaHeader,
+  Content,
+  Counter,
+  CounterText,
+  BagButton,
+  RightSpan,
+  PlusMinusSpan,
+  ItemInfo,
+  ItemImg,
+  ItemContent,
+} from "./StyleComponent";
+import { getItem, ItemType } from "../../mock";
 import MainItem from "../home/MainItem";
 import DeliveryInfo from "./DeliveryInfo";
 import ReturnExchangeInfo from "./ReturnExchangeInfo";
-import { Fold } from "../common";
 
-function EtcInfo(): JSX.Element {
+function InfoSummary(): JSX.Element {
   return (
     <ul>
       <li>
@@ -28,19 +44,72 @@ function EtcInfo(): JSX.Element {
   );
 }
 
+const getPrice = (item: ItemType | undefined): number => {
+  return (
+    (parseInt(item?.price || "0") * (1 - parseInt(item?.sale || "0") / 100)) | 0
+  );
+};
+
 export default function Goods({ goodId }: { goodId: string }): JSX.Element {
   const item = getItem(parseInt(goodId));
+  const price = getPrice(item);
+
+  const [yPercent, setYPercent] = useState({ y: "0%" });
+  const [count, setCount] = useState(1);
 
   return (
-    <div>
+    <div style={{ marginTop: "-15px" }}>
       <MainItem width="100%" {...item}></MainItem>
-      <EtcInfo></EtcInfo>
-      <Fold title="배달 정보">
+      <InfoSummary></InfoSummary>
+      <EtcInfo>
         <DeliveryInfo></DeliveryInfo>
-      </Fold>
-      <Fold style={{ marginTop: "-1px" }} title="반품 및 교환 정보">
         <ReturnExchangeInfo></ReturnExchangeInfo>
-      </Fold>
+      </EtcInfo>
+      <ButtonArea>
+        <Button onClick={(): void => setYPercent({ y: "0%" })}>구매하기</Button>
+      </ButtonArea>
+      <Shadow
+        style={{ display: yPercent.y === "100%" ? "none" : "block" }}
+        onClick={(): void => setYPercent({ y: "100%" })}
+      ></Shadow>
+      <HideArea style={{ transform: `translateY(${yPercent.y})` }}>
+        <HideAreaHeader>
+          <span>{item?.title}</span>
+          <RightSpan onClick={(): void => setYPercent({ y: "100%" })}>
+            닫기
+          </RightSpan>
+        </HideAreaHeader>
+        <Content>
+          <ItemContent>
+            <ItemImg style={{ backgroundImage: `url(${item?.src})` }}></ItemImg>
+
+            <ItemInfo>
+              <ul>
+                <li>{item?.title}</li>
+                <li>{item?.price}</li>
+              </ul>
+            </ItemInfo>
+            <Counter>
+              <PlusMinusSpan
+                onClick={(): void => {
+                  count > 1 && setCount(count - 1);
+                }}
+              >
+                -
+              </PlusMinusSpan>
+              <CounterText>{count}</CounterText>
+              <PlusMinusSpan onClick={(): void => setCount(count + 1)}>
+                +
+              </PlusMinusSpan>
+            </Counter>
+          </ItemContent>
+
+          <BagButton>
+            <span>{count}개 담기 </span>
+            <RightSpan>{count * price}원</RightSpan>
+          </BagButton>
+        </Content>
+      </HideArea>
     </div>
   );
 }
