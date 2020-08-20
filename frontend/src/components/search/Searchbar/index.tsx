8d1944c9829ee2c9c79b4b7c360921e61c5b600f
changicho <changicho29@gmail.com>
2020-08-20 16:29:35 +0900

    refactor: 검색 버튼 위치 변경
    검색 아이콘을 버튼으로 변경하고 요구사항 문서에 맞춰 태그를 변경

--- frontend/src/components/search/Searchbar/index.tsx
+++ frontend/src/components/search/Searchbar/index.tsx
@@ -6,6 +6,8 @@ import Input from "./Input";
 import SearchIcon from "./SeachIcon";
 import DeleteButton from "./DeleteButton";
 
+import getGoodsByName from "../../../fetch/goods/getGoodsByName";
+
 const Wrapper = styled.div`
   z-index: 1000;
   position: fixed;
@@ -18,10 +20,11 @@ const Wrapper = styled.div`
 
 export default function SearchBar(): JSX.Element {
   const [showDelete, setShowDelete] = useState(false);
+  const [query, setQuery] = useState("");
 
   function updateFilter(event: React.KeyboardEvent<HTMLInputElement>): void {
     const filter = (event.target as HTMLInputElement).value;
-
+    setQuery(filter);
     if (filter.length > 0) {
       setShowDelete(true);
     } else {
@@ -40,15 +43,22 @@ export default function SearchBar(): JSX.Element {
       input.value = "";
     }
 
+    setQuery("");
     setShowDelete(false);
   }
 
   return (
     <Wrapper>
       <FixedBox>
-        <SearchIcon />
         <Input placeholder="상품 검색" onKeyUp={updateFilter} />
         <DeleteButton onClick={deleteFilter} show={showDelete} />
+        <SearchIcon
+          onClick={(): void => {
+            getGoodsByName(query).then((res) => {
+              console.log(res);
+            });
+          }}
+        />
       </FixedBox>
     </Wrapper>
   );
