c99c71f1ff02ad2bea61562d547606f2be2ae026
이슬기 <tmfrl002@gmail.com>
2020-08-20 20:23:38 +0900

    style: 프론트엔드 공통 컴포넌트 추가
    - common 엔트리 포인트에 추가

--- frontend/src/components/common/index.ts
+++ frontend/src/components/common/index.ts
@@ -2,5 +2,7 @@ import Layout from "./Layout";
 import SVG from "./Svg";
 import HorizontalSlider from "./Slider/Horizontal";
 import Fold from "./Fold";
+import ShadowBar from "./ShadowBar";
+import CheckBox from "./CheckBox";
 
-export { Layout, SVG, HorizontalSlider, Fold };
+export { Layout, SVG, HorizontalSlider, Fold, ShadowBar, CheckBox };
