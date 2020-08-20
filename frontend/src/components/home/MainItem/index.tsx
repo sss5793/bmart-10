f2248966d975b752e77dd76fc5ad6772e8a6b913
brightchul <abhbtbb@gmail.com>
2020-08-20 14:44:20 +0900

    feat: MainItem에서 클릭시 상세 제품 페이지로 갈수 있도록 변경
    - 이제 개별 상품을 클릭하게 되면 해당 제품 상세페이지로 이동

--- frontend/src/components/home/MainItem/index.tsx
+++ frontend/src/components/home/MainItem/index.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import ItemImg from "./ItemImg";
 import ItemContent from "./ItemContent";
 import { ItemContext, ItemContextType } from "./ItemContext";
+import { useHistory } from "react-router-dom";
 
 const DEFAULT_WIDTH = "107px";
 const wordBreak = "keep-all";
@@ -15,9 +16,13 @@ export default function MainItem({
   width = DEFAULT_WIDTH,
   ...props
 }: ItemContextType): JSX.Element {
+  const history = useHistory();
   return (
     <ItemContext.Provider value={{ width, ...props }}>
-      <ItemArea style={{ width, wordBreak }}>
+      <ItemArea
+        onClick={(): void => history.push(`/goods/${props.goodId}`)}
+        style={{ width, wordBreak }}
+      >
         <ItemImg></ItemImg>
         <ItemContent></ItemContent>
       </ItemArea>
